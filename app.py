9dc26b8739899d41ae1af766618723b91f87c12f
manik singh <109017266+mksgh@users.noreply.github.com>
2025-01-28 01:11:11 +0000

    new functionality added

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app=Flask(__name__)
 def welcome():
     return "Welcome to this best Flask app."
 
-@app.route("/first-page", methods = ['GET'])
+@app.route("/index", methods = ['GET'])
 def index():
     return render_template('index.html')
 
@@ -22,18 +22,35 @@ def about():
 @app.route('/form', methods=['GET','POST'])
 def form():
     if request.method=='POST':
-        name=request.form['name']
-        return f'Hello {name}!'
+        n=request.form['name']
+        m=request.form['marks']
+
+        return f'{n} obtain {m} marks!'
     return render_template('form.html')
 
-@app.route('/submit', methods=['GET','POST'])
-def submit():
+# @app.route('/submit', methods=['GET','POST'])
+# def submit():
+#     if request.method=='POST':
+#         name=request.form['name']
+#         return f'Hello {name}!'
+#     return render_template('form.html')
+
+#variable rule
+@app.route('/success', methods = ['GET','POST'])
+def success():
     if request.method=='POST':
         name=request.form['name']
-        return f'Hello {name}!'
-    return render_template('form.html')
+        score=request.form['marks']
 
-
+        res=""
+        if int(score) >= 50:
+            res="PASSED"
+        else:
+            res="FAILED"
+            
+        return f'Hello {name} you have obtained {score} and you are {res}!'
+    
+    return  render_template('form.html')
 
 if __name__=="__main__":
     app.run(debug=True)
